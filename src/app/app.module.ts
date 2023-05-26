82b81aed56142dfc254a89fe0b0ca3d411f644ab
lev1nn <levin.schaller@bbzbl-it.ch>
2023-05-26 09:35:58 +0200

    Add login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthConfig, OAuthModule, OAuthStorage } from 'angular-oauth2-oidc';
+import { environment } from 'src/environments/environment.development';
+import { HTTP_INTERCEPTORS, HttpClientModule, HttpClientXsrfModule } from '@angular/common/http';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { HttpXSRFInterceptor } from './interceptor/http.csrf.interceptor';
+import { AppAuthGuard } from './guard/app.auth.guard';
+import { AppAuthService } from './service/app.auth.service';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+export const authConfig: AuthConfig = {
+  issuer: 'http://localhost:8080/realms/ILV',
+  requireHttps: false,
+  redirectUri: environment.frontendBaseUrl,
+  postLogoutRedirectUri: environment.frontendBaseUrl,
+  clientId: 'demoapp',
+  scope: 'openid profile roles offline_access',
+  responseType: 'code',
+  showDebugInformation: true,
+  requestAccessToken: true,
+  silentRefreshRedirectUri: window.location.origin + '/silent-refresh.html',
+  silentRefreshTimeout: 500,
+  clearHashAfterLogin: true,
+};
+
+export function storageFactory(): OAuthStorage {
+  return sessionStorage;
+}
 
 @NgModule({
   declarations: [
@@ -10,9 +38,29 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientXsrfModule.withOptions({
+      cookieName: 'XSRF-TOKEN',
+      headerName: 'X-XSRF-TOKEN'
+    }),
+    OAuthModule.forRoot({resourceServer: {sendAccessToken: true}}),
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    {provide: AuthConfig, useValue: authConfig},
+    {provide: HTTP_INTERCEPTORS, useClass: HttpXSRFInterceptor, multi: true},
+    {
+      provide: OAuthStorage, useFactory: storageFactory
+    },
+    AppAuthGuard,
+    Location, {provide: LocationStrategy, useClass: PathLocationStrategy}
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(authService: AppAuthService) {
+    authService.initAuth().finally();
+  }
+}

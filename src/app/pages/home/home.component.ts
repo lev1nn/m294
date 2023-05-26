172a1aed8066494c63819bf490103cd6fe8ad27e
lev1nn <levin.schaller@bbzbl-it.ch>
2023-05-26 16:09:39 +0200

    Create greeting

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {AppAuthService} from '../../service/app.auth.service';
+import {HeaderService} from '../../service/header.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  useralias = '';
+  username = '';
 
+  constructor(private authService: AppAuthService, private headerService: HeaderService) {
+    this.headerService.setPage('nav.dashboard');
+  }
+
+  ngOnInit(): void {
+    this.authService.usernameObservable.subscribe(name => {
+      this.username = name;
+    });
+    this.authService.useraliasObservable.subscribe(alias => {
+      this.useralias = alias;
+    });
+  }
 }

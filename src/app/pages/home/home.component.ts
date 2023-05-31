6b3da3165b40d2ce66831a07b2688a44e8abae7e
lev1nn <levinsch@bluewin.ch>
2023-05-31 17:57:37 +0200

    Update home message

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {AppAuthService} from '../../service/app.auth.service';
 import {HeaderService} from '../../service/header.service';
+import { OAuthService } from 'angular-oauth2-oidc';
 
 @Component({
   selector: 'app-home',
@@ -11,7 +12,7 @@ export class HomeComponent {
   useralias = '';
   username = '';
 
-  constructor(private authService: AppAuthService, private headerService: HeaderService) {
+  constructor(private authService: AppAuthService, private headerService: HeaderService, public oauthService: OAuthService) {
     this.headerService.setPage('nav.dashboard');
   }
 

d67e684fde28c0b4cefaec172602e0d02b039706
lev1nn <levin.schaller@bbzbl-it.ch>
2023-06-02 14:54:29 +0200

    Add component test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,32 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { AuthConfig, OAuthModule } from 'angular-oauth2-oidc';
+import { HttpClientModule } from '@angular/common/http';
+import { authConfig } from './app.module';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        OAuthModule.forRoot({resourceServer: {sendAccessToken: true}}),
+        MatMomentDateModule,
+        MatToolbarModule,
+        HttpClientModule,
+        MatIconModule,
+        MatSidenavModule,
+        BrowserAnimationsModule
       ],
+      providers: [
+        //{provide: HttpClient, useValue: createSpyFromClass(HttpClient)},
+        {provide: AuthConfig, useValue: authConfig}],
       declarations: [
         AppComponent
       ],
@@ -19,4 +38,5 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
+
 });
